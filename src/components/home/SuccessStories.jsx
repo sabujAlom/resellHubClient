c3e9c3ef06b55c24607bc7b5a981d57f11ce6e0b
sabujAlom <sabujalom18@gmail.com>
2026-08-21 14:10:00 +0600

    updeted sucess stories text

--- src/components/home/SuccessStories.jsx
+++ src/components/home/SuccessStories.jsx
@@ -24,7 +24,7 @@ const stories = [
     name: 'Priya Patel',
     role: 'Buyer',
     initials: 'PP',
-    content: '"ResellHub helped me furnish my entire apartment while saving more than $2,000. The quality and prices were incredible!"',
+    content: '"ResellHub helped me furnish my entire apartment while saving mores than $2,000. The quality and prices were incredible!"',
     rating: 5,
   },
   {
